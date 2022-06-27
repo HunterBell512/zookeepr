00caea8651736b47c953cfdb4761a2b87f9269da
Hunter Bell <bellhunter512@gmail.com>
2022-06-27 00:25:14 -0400

    finished MVP

--- server.js
+++ server.js
@@ -44,12 +44,26 @@ var filterByQuery = function (query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-  }
+}
 
-  app.get('/api/animals', (req, res) => {
+var findById = function (id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
-      results = filterByQuery(req.query, results);
+        results = filterByQuery(req.query, results);
     }
     res.json(results);
-  });
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+});
